808ae7c6d5df149d1a1be9b920990d508da886c2
David Thaler <davidthaler@gmail.com>
2015-10-23 18:29:38 -0700

    added as_pandas option for Pandas DataFrame/Series output

--- forecast_wrapper.py
+++ forecast_wrapper.py
@@ -25,31 +25,43 @@ def ts(data, start=1, frequency=1):
   return time_series
 
 
-def extract_forecast(fc, horizon, mean_only):
+def extract_forecast(fc, horizon, mean_only, as_pandas):
   '''
-  Utility function to extract the desired elements from a completed forecast.
+  Utility function to extract the desired elements from a completed forecast 
+  and return them as either Python or Pandas objects.
   
   Args:
     fc - a completed forecast
-    horizon - default 10; number of steps ahead in the forecast
-    mean_only - default False; if True, return only the mean prediction.
+    horizon - number of steps ahead in the forecast
+    mean_only - if True, return only the mean prediction.
+    as_pandas  - if True, return a Pandas DataFrame or Series
     
   Returns:
-    Either the mean forecast as a Python list, or ...         # TODO
+    A forecast with or without prediction intervals, as either a list/tuple
+    or as a Pandas DataFrame/Series.
   '''
   if mean_only:
-    return list(fc.rx2('mean'))
+    result = list(fc.rx2('mean'))
+    if as_pandas:
+      return pd.Series(result)
+    else: 
+      return result
   else:
-    cols = ['lower95','lower80','mean_forecast','upper80','upper95']
     lower_95 = list(fc.rx2('lower')[horizon:])
     lower_80 = list(fc.rx2('lower')[:horizon])
     mean_fc  = list(fc.rx2('mean'))
     upper_80 = list(fc.rx2('upper')[:horizon])
     upper_95 = list(fc.rx2('upper')[horizon:])
-    return (lower_95, lower_80, mean_fc, upper_80, upper_95)
+    results = (lower_95, lower_80, mean_fc, upper_80, upper_95)
+    if as_pandas:
+      cols = ['lower95','lower80','mean_forecast','upper80','upper95']
+      return pd.DataFrame(dict(zip(cols, results)))
+    else:
+      return results
 
 
-def meanf(data, start=1, frequency=1, horizon=10, mean_only=False):
+def meanf(data, start=1, frequency=1, horizon=10, 
+          mean_only=False, as_pandas=True):
   '''
   Perform a mean forecast on the provided data by calling meanf() 
   from R Forecast.
@@ -61,14 +73,16 @@ def meanf(data, start=1, frequency=1, horizon=10, mean_only=False):
         e.g. 12 for monthly data with an annual period
     horizon - default 10; number of steps ahead to forecast
     mean_only - default False; if True, return only the mean prediction.
+    as_pandas  - if True, return a Pandas DataFrame or Series
 
   Returns:
-    Either the mean forecast as a Python list, or ...         # TODO
+    A forecast with or without prediction intervals, as either a list/tuple
+    or as a Pandas DataFrame/Series.
   '''
   time_series = ts(data, start=start, frequency=frequency)
   meanf = robjects.r('meanf')
   fc = meanf(time_series, h=horizon)
-  return extract_forecast(fc, horizon, mean_only)
+  return extract_forecast(fc, horizon, mean_only, as_pandas)
 
 
 

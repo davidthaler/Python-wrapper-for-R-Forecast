3e5714f5cd7f08c6c6ead7205b1a6a0598d4b738
David Thaler <davidthaler@gmail.com>
2015-10-31 01:27:45 -0700

    Initial pandas output with series aligned with the R time series it was made from. Also, index on PI and DC is now correct.

--- extractor.py
+++ extractor.py
@@ -1,5 +1,6 @@
 from rpy2 import robjects
 import pandas as pd
+from math import floor
 
 
 def mean_prediction(fc):
@@ -27,7 +28,8 @@ def prediction_intervals(fc):
     a Pandas DataFrame with the mean prediction and prediction intervals
   '''
   mean_fc = list(fc.rx2('mean'))
-  df = pd.DataFrame({'point_fc' : mean_fc})
+  idx = get_index(fc.rx2('mean'))
+  df = pd.DataFrame({'point_fc' : mean_fc}, index=idx)
   colnames = ['point_fc']
   lower = fc.rx2('lower')
   upper = fc.rx2('upper')
@@ -57,12 +59,12 @@ def decomposition(decomp):
   cls = robjects.r('class')
   if cls(decomp)[0] == 'stl':
     data = decomp.rx2('time.series')
-    r, c = tuple(data.dim)
-    seasonal = list(data[:r])
-    trend = list(data[r:(2*r)])
-    remainder = list(data[(2*r):(3*r)])
+    seasonal = list(data.rx(True, 1))
+    trend = list(data.rx(True, 2))
+    remainder = list(data.rx(True, 3))
     cols = ['seasonal', 'trend', 'remainder']
-    df = pd.DataFrame(dict(zip(cols, (seasonal, trend, remainder))))
+    idx = get_index(data)
+    df = pd.DataFrame(dict(zip(cols, (seasonal, trend, remainder))), index=idx)
     df['data'] = df.sum(axis=1)
     return df[['data', 'seasonal', 'trend', 'remainder']]
   elif cls(decomp)[0] == 'decomposed.ts':
@@ -71,14 +73,51 @@ def decomposition(decomp):
     trend = list(decomp.rx2('trend'))
     remainder = list(decomp.rx2('random'))
     cols = ['data', 'seasonal', 'trend', 'remainder']
-    df = pd.DataFrame(dict(zip(cols, (x, seasonal, trend, remainder))))
+    idx = get_index(decomp.rx2('x'))
+    df = pd.DataFrame(dict(zip(cols, (x, seasonal, trend, remainder))), index=idx)
     return df[cols]
   else:
     raise ValueError('Argument must map to an R seasonal decomposition.')
 
 
+def get_index(ts):
+  '''
+  Utility function for making the correct argument to constructors for 
+  Pandas Series or DataFrame objects so as to get the index to match a 
+  given time series.
+  
+  Args:
+    ts - an object that maps to an R time series (class ts)
+    
+  Returns:
+    either a list or a list of lists
+  '''
+  freq = lambda x : robjects.r('frequency')(x)[0]
+  time = lambda x : list(robjects.r('time')(x))
+  cycle = lambda x : list(robjects.r('cycle')(x))
+  times = [int(floor(x)) for x in time(ts)]
+  cycles = [int(floor(x)) for x in cycle(ts)]
+  if freq(ts) > 1:
+    return [times, cycles]
+  else:
+    return times 
+  
 
+def ts_as_series(ts):
+  '''
+  Convert an R time series into a Pandas Series with the appropriate 
+  (seasonal/non-seasonal) index.
+  
+  Args:
+    ts - an object that maps to an R time series (class ts)
+    
+  Returns:
+    a Pandas Series with the same data and index as ts
+  '''
+  idx = get_index(ts)
+  return pd.Series(ts, index=idx)
 
+   
 
 
 

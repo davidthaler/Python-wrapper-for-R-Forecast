d02f6d4c9e99cbf069b96296cb68e22b22f42649
David Thaler <davidthaler@gmail.com>
2015-12-10 17:35:39 -0800

    updated livestock example to use python regressors for auto_arima

--- examples/livestock.py
+++ examples/livestock.py
@@ -5,15 +5,25 @@ R Forecast package author Rob J. Hyndman.
  
 See: http://robjhyndman.com/hyndsight/piecewise-linear-trends/#more-3413
 '''
+# Not needed if the package is installed
+import sys, os
+sys.path.append(os.path.abspath('..'))
+
 from rforecast import ts_io
 from rforecast import wrappers
 from rforecast import converters
 from rforecast import plots
 
+# This is how to import data that is installed in R.
 stock = ts_io.read_ts('livestock', 'fpp')
 n = len(stock)
-xreg = converters.matrix(range(n))
-newxreg = converters.matrix(range(n, n + 10))
-fc = wrappers.auto_arima(stock, xreg=xreg, newxreg=newxreg)
+fc = wrappers.auto_arima(stock, xreg=range(n), newxreg=range(n, n + 10))
+print 'Australia livestock population 1961-2007'
+print stock
+plots.plot_ts(stock)
+print '10-year forecast of livestock population'
 print fc
-plots.plot_forecast(fc)
+plots.plot_forecast(fc, stock)
+
+
+

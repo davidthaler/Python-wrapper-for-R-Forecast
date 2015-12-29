9a4bc5c58246d075f979929efd431aa03e84399b
David Thaler <davidthaler@gmail.com>
2015-12-28 19:47:12 -0800

    Factored out basic R functions from validate to rbase.

--- rforecast/validate.py
+++ rforecast/validate.py
@@ -1,31 +1,6 @@
 from rpy2 import robjects
+from rbase import cls, dim, colnames
 import pandas
-
-def cls(x):
-  try:
-    return list(robjects.r('class')(x))
-  except NotImplementedError:
-    raise ValueError('Cannot call R function on Python object.')
-
-def colnames(x):
-  try:
-    out = robjects.r('colnames')(x)
-    if out is robjects.NULL:
-      raise ValueError('Object does not have colnames attribute.')
-    else:
-      return list(out)
-  except NotImplementedError:
-    raise ValueError('Cannot call R function on Python object.')
-
-def dim(x):
-  try:
-    out = robjects.r('dim')(x)
-    if out is robjects.NULL:
-      raise ValueError('Object does not have dim attribute.')
-    else:
-      return list(out)
-  except NotImplementedError:
-    raise ValueError('Cannot call R function on Python object.')
     
 def is_R_forecast(fc):
   return type(fc) is robjects.ListVector and 'forecast' in cls(fc)
@@ -38,7 +13,10 @@ def is_Pandas_decomposition(dc):
   col_names = [u'data', u'seasonal', u'trend', u'remainder']
   return (type(dc) is pandas.DataFrame and dc.shape[1] == 4 
           and len(set(dc.columns).intersection(col_names)) == 4)
-  
+
+def is_decomposition(dc):
+  return is_Pandas_decomposition(dc) or is_R_decomposition
+
 def is_R_accuracy(acc):
   if type(acc) is robjects.Matrix:
     cnames = colnames(acc)

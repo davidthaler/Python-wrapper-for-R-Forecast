93839a3edff5995032a9e85013f07bf466842806
David Thaler <davidthaler@gmail.com>
2015-12-28 20:22:45 -0800

    changed ValueError to TypeError in rbase when Python objects are passed

--- rforecast/rbase.py
+++ rforecast/rbase.py
@@ -4,24 +4,24 @@ def cls(x):
   try:
     return list(robjects.r('class')(x))
   except NotImplementedError:
-    raise ValueError('Cannot call R function on Python object.')
+    raise TypeError('Cannot call R function on Python object.')
 
 def colnames(x):
   try:
     out = robjects.r('colnames')(x)
     if out is robjects.NULL:
-      raise ValueError('Object does not have colnames attribute.')
+      raise TypeError('Object does not have colnames attribute.')
     else:
       return list(out)
   except NotImplementedError:
-    raise ValueError('Cannot call R function on Python object.')
+    raise TypeError('Cannot call R function on Python object.')
 
 def dim(x):
   try:
     out = robjects.r('dim')(x)
     if out is robjects.NULL:
-      raise ValueError('Object does not have dim attribute.')
+      raise TypeError('Object does not have dim attribute.')
     else:
       return list(out)
   except NotImplementedError:
-    raise ValueError('Cannot call R function on Python object.')
+    raise TypeError('Cannot call R function on Python object.')

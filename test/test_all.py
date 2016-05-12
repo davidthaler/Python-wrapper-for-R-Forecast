80a8fd8efae9f446c565bf2c8951b0f608830f28
David Thaler <davidthaler@gmail.com>
2016-05-11 18:33:22 -0700

    made test_all invariant to R forecast changes

--- test/test_all.py
+++ test/test_all.py
@@ -1,135 +1,164 @@
 import unittest
 from rforecast import wrappers
+from rforecast import converters
 from rforecast import ts_io
+from rpy2 import robjects
+from rpy2.robjects.packages import importr
 
 
 class EndToEndTestCase(unittest.TestCase):
 
   def setUp(self):
-    self.oil = ts_io.read_series('data/oil.csv')
-    self.aus = ts_io.read_series('data/aus.csv')
-    self.austa = ts_io.read_ts('austa', 'fpp')
+    self.oil_r  = ts_io.read_ts('oil', 'fpp', as_pandas=False)
+    self.oil_py = converters.ts_as_series(self.oil_r)
+    self.aus_r = ts_io.read_ts('austourists', 'fpp', as_pandas=False)
+    self.aus_py = converters.ts_as_series(self.aus_r)
+    self.austa_r = ts_io.read_ts('austa', 'fpp', as_pandas=False)
+    self.austa_py = converters.ts_as_series(self.austa_r)
+    self.fc = importr('forecast')
 
+  def _check_points(self, fc_py, fc_r):
+    '''
+    Checks that the R and python forecasts are the same at select points
+    for both the mean forecast and the prediction intervals. Compares the 
+    first and last values of the mean forecast, and the first value of the 
+    80% confidence lower PI and the last value of the 95% upper PI.
+    
+    Args:
+      fc_py: the python forecast
+      fc_r : the R forecast
+      
+    Return:
+      Nothing, but makes tests assertions which can fail.
+    '''
+    lower = fc_r.rx2('lower')
+    upper = fc_r.rx2('upper')
+    mean  = fc_r.rx2('mean')
+    self.assertAlmostEqual(fc_py.point_fc.iloc[0], mean[0], places=3)
+    self.assertAlmostEqual(fc_py.point_fc.iloc[-1], mean[-1], places=3)
+    self.assertAlmostEqual(fc_py.lower80.iloc[0], lower[0], places=3)
+    self.assertAlmostEqual(fc_py.upper95.iloc[-1], upper[-1], places=3)
+    
   def test_naive(self):
-    fc = wrappers.naive(self.oil)
-    self.assertAlmostEqual(fc.point_fc[2011], 467.7724, places=3)
-    self.assertAlmostEqual(fc.point_fc[2020], 467.7724, places=3)
-    self.assertAlmostEqual(fc.lower80[2011], 404.6370, places=3)
-    self.assertAlmostEqual(fc.upper95[2020], 773.1130, places=3)
+    fc_py = wrappers.naive(self.oil_py)
+    fc_r  = self.fc.naive(self.oil_r)
+    self._check_points(fc_py, fc_r)
     
   def test_thetaf(self):
-    fc = wrappers.thetaf(self.oil)
-    self.assertAlmostEqual(fc.point_fc[2011], 470.9975, places=3)
-    self.assertAlmostEqual(fc.point_fc[2020], 500.0231, places=3)
-    self.assertAlmostEqual(fc.lower80[2011], 408.5509, places=3)
-    self.assertAlmostEqual(fc.upper95[2020], 802.0053, places=3)
+    fc_py = wrappers.thetaf(self.oil_py)
+    fc_r  = self.fc.thetaf(self.oil_r)
+    self._check_points(fc_py, fc_r)
     
   def test_snaive(self):
-    fc = wrappers.snaive(self.aus)
-    self.assertAlmostEqual(fc.point_fc[(2011, 1)], 59.76678, places=3)
-    self.assertAlmostEqual(fc.point_fc[(2012, 4)], 47.91374, places=3)
-    self.assertAlmostEqual(fc.lower80[(2011, 1)], 55.37882, places=3)
-    self.assertAlmostEqual(fc.upper95[(2012, 4)], 57.40424, places=3)
+    fc_py = wrappers.snaive(self.aus_py)
+    fc_r  = self.fc.snaive(self.aus_r)
+    self._check_points(fc_py, fc_r)
   
   def test_rwf(self):
-    fc = wrappers.rwf(self.oil)
-    self.assertAlmostEqual(fc.point_fc[2011], 467.7724, places=3)
-    self.assertAlmostEqual(fc.point_fc[2020], 467.7724, places=3)
-    self.assertAlmostEqual(fc.lower80[2011], 404.7558, places=3)
-    self.assertAlmostEqual(fc.upper95[2020], 772.5385, places=3)
+    fc_py = wrappers.rwf(self.oil_py)
+    fc_r  = self.fc.rwf(self.oil_r)
+    self._check_points(fc_py, fc_r)
 
-  def test_forecast(self):
-    fc = wrappers.forecast(self.oil)
-    self.assertAlmostEqual(fc.point_fc[2011], 467.7721, places=3)
-    self.assertAlmostEqual(fc.point_fc[2020], 467.7721, places=3)
-    self.assertAlmostEqual(fc.lower80[2011], 405.3255, places=3)
-    self.assertAlmostEqual(fc.upper95[2020], 769.7543, places=3)
-    fc = wrappers.forecast(self.aus)
-    self.assertAlmostEqual(fc.point_fc[(2011, 1)], 57.87294, places=3)
-    self.assertAlmostEqual(fc.point_fc[(2012, 4)], 52.84327, places=3)
-    self.assertAlmostEqual(fc.lower80[(2011, 1)], 53.30794, places=3)
-    self.assertAlmostEqual(fc.upper95[(2012, 4)], 62.60852, places=3)
+  def test_forecast_nonseasonal(self):
+    fc_py = wrappers.forecast(self.oil_py)
+    fc_r  = self.fc.forecast(self.oil_r)
+    self._check_points(fc_py, fc_r)
     
-  def test_auto_arima(self):
-    fc = wrappers.auto_arima(self.oil)
-    self.assertAlmostEqual(fc.point_fc[2011], 475.7004, places=3)
-    self.assertAlmostEqual(fc.point_fc[2020], 547.0531, places=3)
-    self.assertAlmostEqual(fc.lower80[2011], 412.6839, places=3)
-    self.assertAlmostEqual(fc.upper95[2020], 851.8193, places=3)
-    fc = wrappers.auto_arima(self.aus)
-    self.assertAlmostEqual(fc.point_fc[(2011, 1)], 60.64208, places=3)
-    self.assertAlmostEqual(fc.point_fc[(2012, 4)], 51.55356, places=3)
-    self.assertAlmostEqual(fc.lower80[(2011, 1)], 57.57010, places=3)
-    self.assertAlmostEqual(fc.upper95[(2012, 4)], 57.52426, places=3)
-    self.assertRaises(ValueError, wrappers.auto_arima, self.oil , 
-                       xreg=range(len(self.oil)))
-    self.assertRaises(ValueError, wrappers.auto_arima, self.oil, h=10, 
+  def test_forecast_seasonal(self):
+    fc_py = wrappers.forecast(self.aus_py)
+    fc_r  = self.fc.forecast(self.aus_r)
+    self._check_points(fc_py, fc_r)
+    
+  def test_auto_arima_nonseasonal(self):
+    fc_py = wrappers.auto_arima(self.oil_py)
+    model = self.fc.auto_arima(self.oil_r)
+    fc_r  = self.fc.forecast(model)
+    self._check_points(fc_py, fc_r)
+
+  def test_auto_arima_seasonal(self):
+    fc_py = wrappers.auto_arima(self.aus_py)
+    model = self.fc.auto_arima(self.aus_r)
+    fc_r  = self.fc.forecast(model)
+    self._check_points(fc_py, fc_r)
+
+  def test_auto_arima_raises(self):
+    self.assertRaises(ValueError, wrappers.auto_arima, self.oil_py , 
+                       xreg=range(len(self.oil_py)))
+    self.assertRaises(ValueError, wrappers.auto_arima, self.oil_py, h=10, 
                        newxreg=range(10))
 
   def test_stlf(self):
-    fc = wrappers.stlf(self.aus)    
-    self.assertAlmostEqual(fc.point_fc[(2011, 1)], 58.88951, places=3)
-    self.assertAlmostEqual(fc.point_fc[(2012, 4)], 51.68499, places=3)
-    self.assertAlmostEqual(fc.lower80[(2011, 1)], 56.69656, places=3)
-    self.assertAlmostEqual(fc.upper95[(2012, 4)], 57.59713, places=3)
+    fc_py = wrappers.stlf(self.aus_py)
+    fc_r  = self.fc.stlf(self.aus_r)
+    self._check_points(fc_py, fc_r)
 
   def test_acf(self):
-    acf = wrappers.acf(self.oil, lag_max=10)
-    self.assertEqual(acf.name, 'Acf')
-    self.assertEqual(len(acf), 10)
-    self.assertAlmostEqual(acf[1], 0.8708, places=3)
-    self.assertAlmostEqual(acf[10], -0.2016, places=3)
+    acf_py = wrappers.acf(self.oil_py, lag_max=10)
+    self.assertEqual(acf_py.name, 'Acf')
+    self.assertEqual(len(acf_py), 10)
+    acf_r = self.fc.Acf(self.oil_r, plot=False, lag_max=10)
+    self.assertAlmostEqual(acf_py[1], acf_r.rx2('acf')[1], places=3)
+    self.assertAlmostEqual(acf_py[10], acf_r.rx2('acf')[10], places=3)
 
   def test_pacf(self):
-    acf = wrappers.pacf(self.oil, lag_max=10)
-    self.assertEqual(acf.name, 'Pacf')
-    self.assertEqual(len(acf), 10)
-    self.assertAlmostEqual(acf[1], 0.8708, places=3)
-    self.assertAlmostEqual(acf[10], 0.1104, places=3)
+    pacf_py = wrappers.pacf(self.oil_py, lag_max=10)
+    self.assertEqual(pacf_py.name, 'Pacf')
+    self.assertEqual(len(pacf_py), 10)
+    pacf_r = self.fc.Pacf(self.oil_r, plot=False, **{'lag.max':10})
+    self.assertAlmostEqual(pacf_py.values[0], pacf_r.rx2('acf')[0], places=3)
+    self.assertAlmostEqual(pacf_py.values[-1], pacf_r.rx2('acf')[-1], places=3)
 
   def test_ses(self):
-    fc = wrappers.ses(self.oil, level=80)
-    self.assertAlmostEqual(fc.point_fc[2011], 467.7724, places=3)
-    self.assertAlmostEqual(fc.point_fc[2020], 467.7724, places=3)
-    self.assertAlmostEqual(fc.lower80[2011], 405.3270, places=3)
-    self.assertAlmostEqual(fc.upper80[2020], 665.2418, places=3)
-    self.assertRaises(ValueError, wrappers.ses, self.oil, alpha=0)
-    self.assertRaises(ValueError, wrappers.ses, self.oil, alpha=1.0)
+    fc_py = wrappers.ses(self.oil_py)
+    fc_r  = self.fc.ses(self.oil_r, initial='simple')
+    self._check_points(fc_py, fc_r)
+
+  def test_ses_raises(self):
+    self.assertRaises(ValueError, wrappers.ses, self.oil_py, alpha=0)
+    self.assertRaises(ValueError, wrappers.ses, self.oil_py, alpha=1.0)
 
   def test_holt(self):
-    fc = wrappers.holt(self.austa)
-    self.assertAlmostEqual(fc.point_fc[2011], 5.5690, places=3)
-    self.assertAlmostEqual(fc.point_fc[2020], 6.7219, places=3)
-    self.assertAlmostEqual(fc.lower80[2011], 5.3316, places=3)
-    self.assertAlmostEqual(fc.upper95[2020], 8.8653, places=3)
-    self.assertRaises(ValueError, wrappers.holt, self.austa, alpha=0)
-    self.assertRaises(ValueError, wrappers.holt, self.austa, alpha=1.0)
-    self.assertRaises(ValueError, wrappers.holt, self.austa, beta=0)
-    self.assertRaises(ValueError, wrappers.holt, self.austa, beta=1.0)
+    fc_py = wrappers.holt(self.austa_py)
+    fc_r  = self.fc.holt(self.austa_r, initial='simple')
+    self._check_points(fc_py, fc_r)
+    
+  def test_holt_raises(self):
+    self.assertRaises(ValueError, wrappers.holt, self.austa_py, alpha=0)
+    self.assertRaises(ValueError, wrappers.holt, self.austa_py, alpha=1.0)
+    self.assertRaises(ValueError, wrappers.holt, self.austa_py, beta=0)
+    self.assertRaises(ValueError, wrappers.holt, self.austa_py, beta=1.0)
 
   def test_hw(self):
-    fc = wrappers.hw(self.aus)
-    self.assertAlmostEqual(fc.point_fc[(2011, 1)], 60.0507, places=3)
-    self.assertAlmostEqual(fc.point_fc[(2012, 4)], 52.2922, places=3)
-    self.assertAlmostEqual(fc.lower80[(2011, 1)], 56.9353, places=3)
-    self.assertAlmostEqual(fc.upper95[(2012, 4)], 64.7920, places=3)
-    self.assertRaises(ValueError, wrappers.hw, self.aus, alpha=0)
-    self.assertRaises(ValueError, wrappers.hw, self.aus, alpha=1.0)
-    self.assertRaises(ValueError, wrappers.hw, self.aus, beta=0)
-    self.assertRaises(ValueError, wrappers.hw, self.aus, beta=1.0)
-    self.assertRaises(ValueError, wrappers.hw, self.aus, gamma=0)
-    self.assertRaises(ValueError, wrappers.hw, self.aus, gamma=1.0)
+    fc_py = wrappers.hw(self.aus_py)
+    fc_r  = self.fc.hw(self.aus_r, initial='simple')
+    self._check_points(fc_py, fc_r)
 
-  def test_arima(self):
-    fc = wrappers.arima(self.aus, order=(1,0,0), seasonal=(1,1,0), 
+  def test_hw_raises(self):
+    self.assertRaises(ValueError, wrappers.hw, self.aus_py, alpha=0)
+    self.assertRaises(ValueError, wrappers.hw, self.aus_py, alpha=1.0)
+    self.assertRaises(ValueError, wrappers.hw, self.aus_py, beta=0)
+    self.assertRaises(ValueError, wrappers.hw, self.aus_py, beta=1.0)
+    self.assertRaises(ValueError, wrappers.hw, self.aus_py, gamma=0)
+    self.assertRaises(ValueError, wrappers.hw, self.aus_py, gamma=1.0)
+    
+  def test_arima_seasonal(self):
+    fc_py = wrappers.arima(self.aus_py, order=(1,0,0), seasonal=(1,1,0), 
                         include_constant=True)
-    self.assertAlmostEqual(fc.point_fc[(2011, 1)], 60.6420, places=3)
-    self.assertAlmostEqual(fc.point_fc[(2012, 4)], 51.5535, places=3)
-    self.assertAlmostEqual(fc.lower80[(2011, 1)], 57.5701, places=3)
-    self.assertAlmostEqual(fc.upper95[(2012, 4)], 57.5242, places=3)
-    self.assertEqual(fc.shape[0], 8)
-    fc = wrappers.arima(self.oil, order=(0,1,0))
-    self.assertEqual(fc.shape[0], 10)
+    order = robjects.r.c(1., 0., 0.)
+    seasonal = robjects.r.c(1., 1., 0.)
+    model = self.fc.Arima(self.aus_r, order=order, seasonal=seasonal, 
+                        include_constant=True)
+    fc_r = self.fc.forecast(model)
+    self._check_points(fc_py, fc_r)
+    self.assertEqual(fc_py.shape[0], 8)
+    
+  def test_arima_nonseasonal(self):
+    fc_py = wrappers.arima(self.oil_py, order=(0,1,0))
+    order = robjects.r.c(0., 1., 0.)
+    model = self.fc.Arima(self.oil_r, order=order)
+    fc_r  = self.fc.forecast(model)
+    self._check_points(fc_py, fc_r)
+    self.assertEqual(fc_py.shape[0], 10)
 
 
 

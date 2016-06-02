17378847599c362c6f5786dc4a1588f640777e2b
David Thaler <davidthaler@gmail.com>
2016-06-02 14:22:21 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name = 'rforecast',
+  version = '0.1',
+  author = 'David Thaler',
+  author_email = 'davidathaler@gmail.com',
+  url = 'https://github.com/davidthaler/Python-wrapper-for-R-Forecast',
+  description = 'A python wrapper to the R forecast package for time series forecasting',
+  license = 'GPL2',
+  packages = ['rforecast']
+)
